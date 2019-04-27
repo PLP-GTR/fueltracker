f34393d9be9e1cc1650d32845a40e37d6dcb3cbb
Philipp Gächter <github@plpgtr.de>
2019-04-27 12:27:43 +0200

    artisan make:model Currency -mcr

---
+++ src/database/migrations/2019_04_27_102703_create_currencies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            // Main information
+            $table->uuid('id')->primary()->comment('Primary key');
+            $table->string('display_value')->comment('Useful to display');
+            $table->string('description')->nullable()->comment('Helpful description to the user');
+
+            // Default
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
