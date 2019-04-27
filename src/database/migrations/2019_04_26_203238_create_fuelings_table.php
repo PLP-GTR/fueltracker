88c03146224cbf93ba9619b0250e1680ffd2be65
Philipp Gächter <github@plpgtr.de>
2019-04-27 10:19:29 +0200

    Base layout of fuelings table

--- src/database/migrations/2019_04_26_203238_create_fuelings_table.php
+++ src/database/migrations/2019_04_26_203238_create_fuelings_table.php
@@ -14,7 +14,19 @@ class CreateFuelingsTable extends Migration
     public function up()
     {
         Schema::create('fuelings', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->uuid('id')->primary()->comment('Primary key');
+            $table->uuid('vehicle_uuid')->comment('Foreign key to vehicles table');
+            $table->dateTimeTz('refueled_at')->comment('When the vehicle was refueled');
+            $table->float('mileage_overall', 7, 2)->nullable()->comment('Overall mileage of the vehicle at fueling');
+            $table->float('mileage_trip', 7, 2)->nullable()->comment('Trip mileage since last fueling');
+            $table->enum('mileage_unit', ['km', 'mi'])->comment('Mileage unit, supported are kilometres and english miles');
+            $table->float('costs')->comment('Costs of the fueling');
+            $table->float('costs_per_volume')->comment('If known, costs per volume can be used to calculate the costs (i.e. if several items are on the receipt)');
+            $table->uuid('currency_uuid')->comment('Foreign key to currencies table; Currency of the fueling costs, i.e. EUR/USD etc');
+            $table->uuid('place_uuid')->comment('Foreign key to places table; Table contains places of map and geocoding providers')
+            $table->uuid('fuel_type_uuid')->comment('Foreign key to fuel types table; Fuel types are Super (95), Super Plus (98) etc');
+            $table->uuid('payment_type_uuid')->comment('Foreign key to payment types table; Payment types are debit card, credit card, cash etc');
+            $table->json('custom_fields')->comment('If the user wants to add more information');
             $table->timestamps();
         });
     }
