8449f06623e80d065ab2c59159d89b6fa5b50b42
Philipp Gächter <github@plpgtr.de>
2019-04-27 12:03:51 +0200

    artisan make:model UtilizationUnit -mcr

---
+++ src/app/Http/Controllers/UtilizationUnitController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\UtilizationUnit;
+use Illuminate\Http\Request;
+
+class UtilizationUnitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\UtilizationUnit  $utilizationUnit
+     * @return \Illuminate\Http\Response
+     */
+    public function show(UtilizationUnit $utilizationUnit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\UtilizationUnit  $utilizationUnit
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(UtilizationUnit $utilizationUnit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\UtilizationUnit  $utilizationUnit
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, UtilizationUnit $utilizationUnit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\UtilizationUnit  $utilizationUnit
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(UtilizationUnit $utilizationUnit)
+    {
+        //
+    }
+}
--- src/database/migrations/2019_04_26_203238_create_fuelings_table.php
+++ src/database/migrations/2019_04_26_203238_create_fuelings_table.php
@@ -16,17 +16,25 @@ class CreateFuelingsTable extends Migration
         Schema::create('fuelings', function (Blueprint $table) {
             $table->uuid('id')->primary()->comment('Primary key');
             $table->uuid('vehicle_uuid')->comment('Foreign key to vehicles table');
-            $table->dateTimeTz('refueled_at')->comment('When the vehicle was refueled');
+            $table->dateTimeTz('refueled_at')->comment('When the vehicle was refueled, receipt date');
+
+            // Uitilization / Milage
             $table->float('utilization_overall', 7, 2)->nullable()->comment('Overall utilization of the vehicle at fueling');
             $table->float('utilization_trip', 7, 2)->nullable()->comment('Trip/partial utilization since last fueling');
             $table->uuid('utilization_unit')->comment('Foreign key to distance units talbe; Common ones are km/mi/nm/h');
+
+            // Costs
             $table->float('costs')->comment('Costs of the fueling');
             $table->float('costs_per_capacity')->comment('If known, costs per 1 capacity can be used to calculate the costs (i.e. if several items are on the receipt)');
             $table->uuid('currency_uuid')->comment('Foreign key to currencies table; Currency of the fueling costs, i.e. EUR/USD etc');
-            $table->uuid('place_uuid')->comment('Foreign key to places table; Table contains places of map and geocoding providers');
+
+            // Types and other things
             $table->uuid('fuel_type_uuid')->comment('Foreign key to fuel types table; Fuel types are Super (95), Super Plus (98) etc');
             $table->uuid('payment_type_uuid')->comment('Foreign key to payment types table; Payment types are debit card, credit card, cash etc');
+            $table->uuid('place_uuid')->comment('Foreign key to places table; Table contains places of map and geocoding providers');
             $table->json('custom_fields')->comment('If the user wants to add more information');
+
+            // Default
             $table->timestamps();
         });
     }
