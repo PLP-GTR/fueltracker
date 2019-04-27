60aa9ff5f6f922e55969bb6f9c648b29a6024002
Philipp Gächter <github@plpgtr.de>
2019-04-27 17:42:48 +0200

    Added Vehicle and Tank relations

--- src/app/Tank.php
+++ src/app/Tank.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tank extends Model
 {
-    //
+    
+    /**
+     * Retrieve the vehicle the tank belongs to
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function vehicle()
+    {
+        return $this->hasOne(Vehicle::class);
+    }
 }
--- src/app/Vehicle.php
+++ src/app/Vehicle.php
@@ -2,9 +2,63 @@
 
 namespace App;
 
+use App\ConsumptionUnit;
+use App\FuelUnit;
+use App\Tank;
+use App\User;
+use App\UtilizationUnit;
 use Illuminate\Database\Eloquent\Model;
 
 class Vehicle extends Model
 {
-    //
+    
+    /**
+     * Retrieve the user the vehicle belongs to
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
+
+    /**
+     * Retrieve the tanks which belong to the vehicle
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function tanks()
+    {
+        return $this->hasMany(Tanks::class);
+    }
+    
+    /**
+     * Retrieve the utilization unit the vehicle belongs to
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function utilizationUnit()
+    {
+        return $this->hasOne(UtilizationUnit::class);
+    }
+    
+    /**
+     * Retrieve the fuel unit the vehicle belongs to
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function fuelUnit()
+    {
+        return $this->hasOne(FuelUnit::class);
+    }
+    
+    /**
+     * Retrieve the consumption unit the vehicle belongs to
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function consumptionUnit()
+    {
+        return $this->hasOne(ConsumptionUnit::class);
+    }
 }
