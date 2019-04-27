4a29f0b1737a75e91075678ebecf311387cbd6ad
Philipp Gächter <github@plpgtr.de>
2019-04-27 17:50:20 +0200

    artisan make:seeder CapactiyUnitsSeeder

---
+++ src/database/seeds/CapactiyUnitsSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CapactiyUnitsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
