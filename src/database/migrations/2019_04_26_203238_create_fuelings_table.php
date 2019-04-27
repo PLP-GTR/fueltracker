f640c9580209e4e22ec3ddd44b13f81ad08082bf
Philipp Gächter <github@plpgtr.de>
2019-04-27 10:45:08 +0200

    Syntax error

--- src/database/migrations/2019_04_26_203238_create_fuelings_table.php
+++ src/database/migrations/2019_04_26_203238_create_fuelings_table.php
@@ -23,7 +23,7 @@ class CreateFuelingsTable extends Migration
             $table->float('costs')->comment('Costs of the fueling');
             $table->float('costs_per_volume')->comment('If known, costs per volume can be used to calculate the costs (i.e. if several items are on the receipt)');
             $table->uuid('currency_uuid')->comment('Foreign key to currencies table; Currency of the fueling costs, i.e. EUR/USD etc');
-            $table->uuid('place_uuid')->comment('Foreign key to places table; Table contains places of map and geocoding providers')
+            $table->uuid('place_uuid')->comment('Foreign key to places table; Table contains places of map and geocoding providers');
             $table->uuid('fuel_type_uuid')->comment('Foreign key to fuel types table; Fuel types are Super (95), Super Plus (98) etc');
             $table->uuid('payment_type_uuid')->comment('Foreign key to payment types table; Payment types are debit card, credit card, cash etc');
             $table->json('custom_fields')->comment('If the user wants to add more information');
