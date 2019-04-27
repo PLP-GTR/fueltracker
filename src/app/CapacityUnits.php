3d2f3464c385d54b8150ab287df02acfa83b3454
Philipp Gächter <github@plpgtr.de>
2019-04-27 12:17:45 +0200

    artisan make:model CapacityUnits -mcr

---
+++ src/app/CapacityUnits.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CapacityUnits extends Model
+{
+    //
+}
---
+++ src/app/Http/Controllers/CapacityUnitsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\CapacityUnits;
+use Illuminate\Http\Request;
+
+class CapacityUnitsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\CapacityUnits  $capacityUnits
+     * @return \Illuminate\Http\Response
+     */
+    public function show(CapacityUnits $capacityUnits)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\CapacityUnits  $capacityUnits
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(CapacityUnits $capacityUnits)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\CapacityUnits  $capacityUnits
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, CapacityUnits $capacityUnits)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\CapacityUnits  $capacityUnits
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CapacityUnits $capacityUnits)
+    {
+        //
+    }
+}
