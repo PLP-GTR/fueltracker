c44a2cf31ec0370d047db692fec0d8ee1d48b648
Philipp Gächter <github@plpgtr.de>
2019-04-28 12:49:08 +0200

    Added base layout of cross table between fuel categories and types

--- src/database/migrations/2019_04_28_104109_create_fuel_category_fuel_types_table.php
+++ src/database/migrations/2019_04_28_104109_create_fuel_category_fuel_types_table.php
@@ -14,7 +14,11 @@ class CreateFuelCategoryFuelTypesTable extends Migration
     public function up()
     {
         Schema::create('fuel_category_fuel_types', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->uuid('id')->primary()->comment('Primary key');
+
+            $table->uuid('fuel_category_id')->comment('Foreign key to fuel categories table');
+            $table->uuid('fuel_type_id')->comment('Foreign key to fuel types table');
+
             $table->timestamps();
         });
     }
