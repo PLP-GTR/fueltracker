957826000edd5997ec72e0edd3c45eb344c64024
Philipp Gächter <github@plpgtr.de>
2019-05-03 21:51:43 +0200

    Can't get foreign key constraints to work, had to re-order the migration scripts

--- src/database/migrations/2019_04_26_010000_create_fuelings_table.php
+++ src/database/migrations/2019_04_26_010000_create_fuelings_table.php
@@ -21,7 +21,7 @@ class CreateFuelingsTable extends Migration
             // Uitilization / Milage
             $table->float('utilization_overall', 7, 2)->nullable()->comment('Overall utilization of the vehicle at fueling');
             $table->float('utilization_trip', 7, 2)->nullable()->comment('Trip/partial utilization since last fueling');
-            $table->uuid('utilization_unit')->comment('Foreign key to distance units talbe; Common ones are km/mi/nm/h');
+            $table->uuid('utilization_unit_id')->comment('Foreign key to distance units talbe; Common ones are km/mi/nm/h');
 
             // Costs
             $table->float('costs')->comment('Costs of the fueling');
@@ -29,7 +29,7 @@ class CreateFuelingsTable extends Migration
             $table->uuid('currency_id')->comment('Foreign key to currencies table; Currency of the fueling costs, i.e. EUR/USD etc');
 
             // Types and other things
-            $table->uuid('fuel_subtype_id')->comment('Foreign key to fuel subtypes table; Fuel subtypes are Super (95), Super Plus (98) etc');
+            $table->uuid('fuel_type_id')->comment('Foreign key to fuel subtypes table; Fuel subtypes are Super (95), Super Plus (98) etc');
             $table->uuid('payment_type_id')->comment('Foreign key to payment types table; Payment types are debit card, credit card, cash etc');
             $table->uuid('capacity_unit_id')->comment('Foreign key to capacity types table; Examples are liter, gallons etc');
             $table->uuid('place_id')->comment('Foreign key to places table; Table contains places of map and geocoding providers');
@@ -37,6 +37,15 @@ class CreateFuelingsTable extends Migration
 
             // Default
             $table->timestamps();
+            
+            // Foreign Key Constraints
+            $table->foreign('vehicle_id')->references('id')->on('vehicles')->change();
+            $table->foreign('utilization_unit_id')->references('id')->on('utilization_units')->change();
+            $table->foreign('currency_id')->references('id')->on('currencies')->change();
+            $table->foreign('fuel_type_id')->references('id')->on('fuel_types')->change();
+            $table->foreign('payment_type_id')->references('id')->on('payment_types')->change();
+            $table->foreign('capacity_unit_id')->references('id')->on('capacity_units')->change();
+            $table->foreign('place_id')->references('id')->on('places')->change();
         });
     }
 
