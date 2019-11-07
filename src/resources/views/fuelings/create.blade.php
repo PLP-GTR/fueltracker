336f41dfa7e4eb1dc3e403536ef8a10ce2500d5c
Philipp Gächter <github@plpgtr.de>
2019-11-07 23:47:51 +0100

    Formatted fueling forms

--- src/resources/views/fuelings/create.blade.php
+++ src/resources/views/fuelings/create.blade.php
@@ -32,6 +32,18 @@
                             <div class="col-sm-8">{{ Form::date('refueled_at', \Carbon\Carbon::now(), ['class' => 'form-control']) }}</div>
                         </div>
                         <div class="row align-items-center mt-2">
+                            <div class="col-sm-4">{{ Form::label('amount', 'Amount') }}</div>
+                            <div class="col-sm-8">{{ Form::number('amount', null, ['placeholder' => '55.67', 'step' => '0.01', 'class' => 'form-control']) }}</div>
+                        </div>
+                        <div class="row align-items-center mt-2">
+                            <div class="col-sm-4">{{ Form::label('capacity_unit_id', 'Capacity Unit') }}</div>
+                            <div class="col-sm-8">{{ Form::select('capacity_unit_id', App\CapacityUnit::all()->pluck('human_readable','id'), null, ['placeholder' => 'Choose...', 'class' => 'form-control']) }}</div>
+                        </div>
+                        <div class="row align-items-center mt-2">
+                            <div class="col-sm-4">{{ Form::label('fuel_type_id', 'Fuel Type') }}</div>
+                            <div class="col-sm-8">{{ Form::select('fuel_type_id', App\FuelType::all()->pluck('human_readable','id'), null, ['placeholder' => 'Choose...', 'class' => 'form-control']) }}</div>
+                        </div>
+                        <div class="row align-items-center mt-5">
                             <div class="col-sm-4">{{ Form::label('currency_id', 'Currency') }}</div>
                             <div class="col-sm-8">{{ Form::select('currency_id', App\Currency::all()->pluck('display_value','id'), null, ['placeholder' => 'Choose...', 'class' => 'form-control']) }}</div>
                         </div>
@@ -40,38 +52,26 @@
                             <div class="col-sm-8">{{ Form::number('costs', null, ['placeholder' => '68.89', 'step' => '0.01', 'class' => 'form-control']) }}</div>
                         </div>
                         <div class="row align-items-center mt-2">
-                            <div class="col-sm-4">{{ Form::label('amount', 'Amount') }}</div>
-                            <div class="col-sm-8">{{ Form::number('amount', null, ['placeholder' => '55.67', 'step' => '0.01', 'class' => 'form-control']) }}</div>
-                        </div>
-                        <div class="row align-items-center mt-2">
-                            <div class="col-sm-4">{{ Form::label('costs_per_capacity', 'Costs per capacity') }}<br/><i>i.e. USD per gallon, euro per liter</i></div>
+                            <div class="col-sm-4"><p>{{ Form::label('costs_per_capacity', 'Costs per capacity') }}<br/><i>i.e. USD per gallon, euro per liter</i></p></div>
                             <div class="col-sm-8">{{ Form::number('costs_per_capacity', null, ['placeholder' => '4.599', 'step' => '0.001', 'class' => 'form-control']) }}</div>
                         </div>
                         <div class="row align-items-center mt-2">
-                            <div class="col-sm-4">{{ Form::label('utilization_overall', 'Car utilization overall') }}<br/><i>Milage or usage hours</i></div>
+                            <div class="col-sm-4">{{ Form::label('payment_type_id', 'Payment Type') }}</div>
+                            <div class="col-sm-8">{{ Form::select('payment_type_id', App\PaymentType::all()->pluck('display_value','id'), null, ['placeholder' => 'Choose...', 'class' => 'form-control']) }}</div>
+                        </div>
+                        <div class="row align-items-center mt-5">
+                            <div class="col-sm-4"><p>{{ Form::label('utilization_overall', 'Car utilization overall') }}<br/><i>Milage or usage hours</i></p></div>
                             <div class="col-sm-8">{{ Form::number('utilization_overall', null, ['placeholder' => '2258', 'class' => 'form-control']) }}</div>
                         </div>
                         <div class="row align-items-center mt-2">
-                            <div class="col-sm-4">{{ Form::label('utilization_trip', 'Trip utilization') }}<br/><i>Milage or usage hours</i></div>
+                            <div class="col-sm-4"><p>{{ Form::label('utilization_trip', 'Trip utilization') }}<br/><i>Milage or usage hours</i></p></div>
                             <div class="col-sm-8">{{ Form::number('utilization_trip', null, ['placeholder' => '124.5', 'step' => '0.1', 'class' => 'form-control']) }}</div>
                         </div>
                         <div class="row align-items-center mt-2">
                             <div class="col-sm-4">{{ Form::label('utilization_unit_id', 'Utilization unit') }}</div>
                             <div class="col-sm-8">{{ Form::select('utilization_unit_id', App\UtilizationUnit::all()->pluck('human_readable','id'), null, ['placeholder' => 'Choose...', 'class' => 'form-control']) }}</div>
                         </div>
-                        <div class="row align-items-center mt-2">
-                            <div class="col-sm-4">{{ Form::label('fuel_type_id', 'Fuel Type') }}</div>
-                            <div class="col-sm-8">{{ Form::select('fuel_type_id', App\FuelType::all()->pluck('human_readable','id'), null, ['placeholder' => 'Choose...', 'class' => 'form-control']) }}</div>
-                        </div>
-                        <div class="row align-items-center mt-2">
-                            <div class="col-sm-4">{{ Form::label('payment_type_id', 'Payment Type') }}</div>
-                            <div class="col-sm-8">{{ Form::select('payment_type_id', App\PaymentType::all()->pluck('display_value','id'), null, ['placeholder' => 'Choose...', 'class' => 'form-control']) }}</div>
-                        </div>
-                        <div class="row align-items-center mt-2">
-                            <div class="col-sm-4">{{ Form::label('capacity_unit_id', 'Capacity Unit') }}</div>
-                            <div class="col-sm-8">{{ Form::select('capacity_unit_id', App\CapacityUnit::all()->pluck('human_readable','id'), null, ['placeholder' => 'Choose...', 'class' => 'form-control']) }}</div>
-                        </div>
-                        <div class="row align-items-center mt-2">
+                        <div class="row align-items-center mt-5">
                             <div class="col-sm-4">{{ Form::label('place_id', 'Place') }}</div>
                             <div class="col-sm-8">{{ Form::select('place_id', App\Place::all()->pluck('name','id'), null, ['placeholder' => 'Choose...', 'class' => 'form-control']) }}</div>
                         </div>
@@ -79,12 +79,22 @@
                             <div class="col-sm-4">{{ Form::label('custom_fields', 'Custom fields') }}</div>
                             <div class="col-sm-8">{{ Form::textarea('custom_fields', null, ['placeholder' => "One:Value\nper:line", 'class' => 'form-control']) }}</div>
                         </div>
-                        <div class="row align-items-center mt-2">
-                            <div class="col-sm-4">{{ Form::submit('Add fueling', array('class' => 'btn btn-primary float-right')) }}</div>
-                            <div class="col-sm-8"></div>
+
+                        <div class="row mt-5">
+                            <div class="col-sm-3">{{ Form::submit('Add fueling', array('class' => 'btn btn-primary float-left')) }}</div>
+                            <div class="col-sm-9"><a class="btn btn-link text-secondary" href="{{ route('vehicles.fuelings.index', $vehicle->id) }}">Cancel creation</a></div>
                         </div>
 
                     {{ Form::close() }}
+
+                    @if ($errors->any())
+                        <div class="row mt-3">
+                            <div class="col text-danger">Please check your input:</div>
+                        </div>
+                        @foreach ($errors->all() as $error)
+                            <div class="row"><div class="col">{{$error}}</div></div>
+                        @endforeach
+                    @endif
 
                 </div>
             </div>
--- src/resources/views/fuelings/edit.blade.php
+++ src/resources/views/fuelings/edit.blade.php
@@ -26,6 +26,18 @@
                             <div class="col-sm-8">{{ Form::date('refueled_at', \Carbon\Carbon::now(), ['class' => 'form-control']) }}</div>
                         </div>
                         <div class="row align-items-center mt-2">
+                            <div class="col-sm-4">{{ Form::label('amount', 'Amount') }}</div>
+                            <div class="col-sm-8">{{ Form::number('amount', null, ['placeholder' => '55.67', 'step' => '0.01', 'class' => 'form-control']) }}</div>
+                        </div>
+                        <div class="row align-items-center mt-2">
+                            <div class="col-sm-4">{{ Form::label('capacity_unit_id', 'Capacity Unit') }}</div>
+                            <div class="col-sm-8">{{ Form::select('capacity_unit_id', App\CapacityUnit::all()->pluck('human_readable','id'), null, ['placeholder' => 'Choose...', 'class' => 'form-control']) }}</div>
+                        </div>
+                        <div class="row align-items-center mt-2">
+                            <div class="col-sm-4">{{ Form::label('fuel_type_id', 'Fuel Type') }}</div>
+                            <div class="col-sm-8">{{ Form::select('fuel_type_id', App\FuelType::all()->pluck('human_readable','id'), null, ['placeholder' => 'Choose...', 'class' => 'form-control']) }}</div>
+                        </div>
+                        <div class="row align-items-center mt-5">
                             <div class="col-sm-4">{{ Form::label('currency_id', 'Currency') }}</div>
                             <div class="col-sm-8">{{ Form::select('currency_id', App\Currency::all()->pluck('display_value','id'), null, ['placeholder' => 'Choose...', 'class' => 'form-control']) }}</div>
                         </div>
@@ -34,14 +46,14 @@
                             <div class="col-sm-8">{{ Form::number('costs', null, ['placeholder' => '68.89', 'step' => '0.01', 'class' => 'form-control']) }}</div>
                         </div>
                         <div class="row align-items-center mt-2">
-                            <div class="col-sm-4">{{ Form::label('amount', 'Amount') }}</div>
-                            <div class="col-sm-8">{{ Form::number('amount', null, ['placeholder' => '55.67', 'step' => '0.01', 'class' => 'form-control']) }}</div>
-                        </div>
-                        <div class="row align-items-center mt-2">
                             <div class="col-sm-4"><p>{{ Form::label('costs_per_capacity', 'Costs per capacity') }}<br/><i>i.e. USD per gallon, euro per liter</i></p></div>
                             <div class="col-sm-8">{{ Form::number('costs_per_capacity', null, ['placeholder' => '4.599', 'step' => '0.001', 'class' => 'form-control']) }}</div>
                         </div>
                         <div class="row align-items-center mt-2">
+                            <div class="col-sm-4">{{ Form::label('payment_type_id', 'Payment Type') }}</div>
+                            <div class="col-sm-8">{{ Form::select('payment_type_id', App\PaymentType::all()->pluck('display_value','id'), null, ['placeholder' => 'Choose...', 'class' => 'form-control']) }}</div>
+                        </div>
+                        <div class="row align-items-center mt-5">
                             <div class="col-sm-4"><p>{{ Form::label('utilization_overall', 'Car utilization overall') }}<br/><i>Milage or usage hours</i></p></div>
                             <div class="col-sm-8">{{ Form::number('utilization_overall', null, ['placeholder' => '2258', 'class' => 'form-control']) }}</div>
                         </div>
@@ -53,19 +65,7 @@
                             <div class="col-sm-4">{{ Form::label('utilization_unit_id', 'Utilization unit') }}</div>
                             <div class="col-sm-8">{{ Form::select('utilization_unit_id', App\UtilizationUnit::all()->pluck('human_readable','id'), null, ['placeholder' => 'Choose...', 'class' => 'form-control']) }}</div>
                         </div>
-                        <div class="row align-items-center mt-2">
-                            <div class="col-sm-4">{{ Form::label('fuel_type_id', 'Fuel Type') }}</div>
-                            <div class="col-sm-8">{{ Form::select('fuel_type_id', App\FuelType::all()->pluck('human_readable','id'), null, ['placeholder' => 'Choose...', 'class' => 'form-control']) }}</div>
-                        </div>
-                        <div class="row align-items-center mt-2">
-                            <div class="col-sm-4">{{ Form::label('payment_type_id', 'Payment Type') }}</div>
-                            <div class="col-sm-8">{{ Form::select('payment_type_id', App\PaymentType::all()->pluck('display_value','id'), null, ['placeholder' => 'Choose...', 'class' => 'form-control']) }}</div>
-                        </div>
-                        <div class="row align-items-center mt-2">
-                            <div class="col-sm-4">{{ Form::label('capacity_unit_id', 'Capacity Unit') }}</div>
-                            <div class="col-sm-8">{{ Form::select('capacity_unit_id', App\CapacityUnit::all()->pluck('human_readable','id'), null, ['placeholder' => 'Choose...', 'class' => 'form-control']) }}</div>
-                        </div>
-                        <div class="row align-items-center mt-2">
+                        <div class="row align-items-center mt-5">
                             <div class="col-sm-4">{{ Form::label('place_id', 'Place') }}</div>
                             <div class="col-sm-8">{{ Form::select('place_id', App\Place::all()->pluck('name','id'), null, ['placeholder' => 'Choose...', 'class' => 'form-control']) }}</div>
                         </div>
